0469fc643bcfc17e283bc40d12cae88632e2645a
NicoZeiss <zeissern@gmail.com>
2024-04-28 12:18:34 +0200

    Add some responsivness

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
 export class Strings {
   static readonly siteTitle = "Julie Zeisser";
-  static readonly logoBaseline = "Traductrice spécialisée en histoire et archéologie";
+  static readonly logoBaseline = "Traductrice indépendante anglais, italien > français";
+
+  static readonly homeSectionBaseline = "Traduction et correction spécialisée en sciences humaines et sociales, informatique, pédagogie";
 }
